de19d94b3f79684e5f3684cdaa60331d4efa3b38
Maksim <maksimus.tumannick@yandex.ru>
2022-06-29 11:51:20 +0300

    Task4.2

---
+++ Task4.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int helthBar = 8;
+            int maxHealthBar = 10;
+            int manaBar = 6;
+            int maxManaBar = 10;
+            int position = 0;
+            DrawBar(helthBar, maxHealthBar, ConsoleColor.Red, 0);
+            position++;
+            DrawBar(manaBar, maxManaBar, ConsoleColor.Blue, position);
+        }
+
+        static void DrawBar(int value, int maxValue, ConsoleColor color, int position)
+        {
+            ConsoleColor defaultColor = Console.BackgroundColor;
+            string bar = "";
+
+            for (int i = 0; i < value; i++)
+            {
+                bar += " ";
+            }
+
+            Console.SetCursorPosition(0, position);
+            Console.Write("[");
+            Console.BackgroundColor = color;
+            Console.Write(bar);
+            Console.BackgroundColor = defaultColor;
+            bar = "";
+
+            for (int i = value; i < maxValue; i++)
+            {
+                bar += " ";
+            }
+
+            Console.Write(bar + "]\n");
+        }
+    }
+}
